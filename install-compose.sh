e75cd0a99dd767350c0120b912f1e131a42a3b99
piechota1989 <piechota1989@gmail.com>
2020-01-09 12:13:26 +0000

    Docker installation packages

---
+++ install-compose.sh
@@ -0,0 +1,9 @@
+# Install required packages
+sudo apt update
+sudo apt install -y libzbar-dev libzbar0
+sudo apt install -y python python-pip libffi-dev python-backports.ssl-match-hostname
+
+# Install Docker Compose from pip
+# This might take a while
+sudo apt install python3-pip
+sudo pip3 install docker-compose
---
+++ install-docker.sh
@@ -0,0 +1,26 @@
+# Install some required packages first
+sudo apt update
+sudo apt install -y \
+     apt-transport-https \
+     ca-certificates \
+     curl \
+     gnupg2 \
+     software-properties-common
+
+# Get the Docker signing key for packages
+curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | sudo apt-key add -
+
+# Add the Docker official repos
+echo "deb [arch=armhf] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \
+     $(lsb_release -cs) stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list
+
+# Install Docker
+# The aufs package, part of the "recommended" packages, won't install on Buster just yet, because of missing pre-compiled kernel modules.
+# We can work around that issue by using "--no-install-recommends"
+sudo apt update
+sudo apt install -y --no-install-recommends \
+    docker-ce \
+    cgroupfs-mount
+
+sudo usermod -aG docker $USER
