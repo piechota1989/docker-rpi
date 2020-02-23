b368b2d0510e0f011d1eca57fca1260326e024ce
piechota1989 <40562553+piechota1989@users.noreply.github.com>
2020-02-23 15:03:23 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,11 +15,28 @@ do
 done
 echo '----------------------------'
 
-sudo apt install $packets
+echo sudo apt install -y $packets
 
 if [[ ! -d "./docker-rpi" ]]
 then
         git clone https://github.com/piechota1989/docker-rpi
 else
         echo "Directory exists"
+fi
+
+test1=$(echo "d83b72d907c209fb4be9fc60b1fc99de817610ddc39d8227e7051987ac4fd1ad  docker-rpi/install-docker.sh" | sha256sum --check | awk '{print $NF}')
+test2=$(echo "336dca5cb2aa6f9014ba97eb0e790bdf9bface31696b50cad62982db4066dd3d  docker-rpi/install-compose.sh" | sha256sum --check | awk '{print $NF}')
+
+if [[ $test1 == "OK" ]]
+then
+        echo "OK"
+else
+        exit
+fi
+
+if [[ $test2 == "OK" ]]
+then
+        echo "OK"
+else
+        exit
 fi
